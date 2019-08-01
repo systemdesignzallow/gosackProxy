2788f3b1f6627d3b2a0ec2b51be754ad4db9d44d
Patrick Gosack <gosack@gmail.com>
2019-08-01 15:23:32 -0700

    server route setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+// do env vars
 
 app.use(express.static("./public"));
+
+app.get("/houses/:houseId", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.listen(3000, () => {
   console.log("proxy on http://localhost:3000");
