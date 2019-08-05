3b619096b5958d60e53d0f4c64c7719929737685
Patrick Gosack <gosack@gmail.com>
2019-08-05 10:25:40 -0700

    bootstrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,16 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo yum install git -y
+
 cd ~
 git clone https://github.com/systemdesignzallow/gosackProxy
 cd gosackProxy
+git checkout docker
+
+echo 'LOCAL_SERVICE_SERVER_PORT=6003' >> .env
+echo 'NODE_ENV="docker"' >> .env
+echo 'NEW_RELIC="ab77636d6d1a81e91b0b1326702da84ae52124a9"' >> .env
+
 sudo docker pull node:11.15.0
 sudo docker build -t gosackproxy .
-sudo docker run -d --name newproxy -p 6000:6000 gosackproxy
+sudo docker run -itd -p 80:6003 --name newproxy gosackproxy
