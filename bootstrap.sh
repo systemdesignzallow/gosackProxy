24c2c1c037533b5e6fc85aca5ef997569cc78d01
Patrick Gosack <gosack@gmail.com>
2019-08-05 15:17:16 -0700

    add vim for ss edits

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo yum install git -y
+sudo yum install vim -y
 
 cd ~
 git clone https://github.com/systemdesignzallow/gosackProxy
