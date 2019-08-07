5f83e75b458caa6dcd4e02930b43a6a7d6320761
Patrick Gosack <gosack@gmail.com>
2019-08-07 09:43:56 -0700

    cleanup

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -15,7 +15,6 @@ git checkout docker
 
 echo 'LOCAL_SERVICE_SERVER_PORT=6003' >> .env
 echo 'NODE_ENV="docker"' >> .env
-echo 'NEW_RELIC="ab77636d6d1a81e91b0b1326702da84ae52124a9"' >> .env
 
 sudo docker pull node:11.15.0
 sudo docker build -t gosackproxy .
